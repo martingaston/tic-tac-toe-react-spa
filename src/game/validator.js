9e236b293c44e0d3aba9aa7c072b087c83974f15
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-30 17:12:07 +0100

    add game logic to src

---
+++ src/game/validator.js
@@ -0,0 +1,26 @@
+const referee = require('./referee')
+const messages = require('./messages')
+
+module.exports = (input, board) => {
+  const base = 10
+  const parsedInput = Number.parseInt(input, base)
+
+  if (Number.isNaN(parsedInput)) {
+    return createResponse('notANumber', messages.nan)
+  }
+
+  if (!referee.withinBounds(parsedInput, board)) {
+    return createResponse('outOfBounds', messages.outOfBounds)
+  }
+
+  if (!referee.available(board).includes(parsedInput)) {
+    return createResponse('occupied', messages.occupied)
+  }
+
+  return createResponse('ok', '')
+}
+
+const createResponse = (status, message) => ({
+  status,
+  message,
+})
