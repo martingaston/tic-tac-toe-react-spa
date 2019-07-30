6886430587a12529d6ae69541a963043bda68a1d
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-30 11:27:59 +0100

    add more styling and font

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ function App() {
     <div className="App">
       <div className="board">
         <div className='row'>
-          <div className='square'>1</div>
-          <div className='square'>2</div>
-          <div className='square'>3</div>
+          <div className='square player-one'>X</div>
+          <div className='square player-two'>O</div>
+          <div className='square player-one'>X</div>
         </div>
         <div className='row'>
-          <div className='square'>4</div>
-          <div className='square'>5</div>
-          <div className='square'>6</div>
+          <div className='square player-two'>O</div>
+          <div className='square player-one'>X</div>
+          <div className='square player-two'>O</div>
         </div>
         <div className='row'>
-          <div className='square'>7</div>
-          <div className='square'>8</div>
-          <div className='square'>9</div>
+          <div className='square player-one'>X</div>
+          <div className='square player-two'>O</div>
+          <div className='square available'></div>
         </div>
       </div>
     </div>
