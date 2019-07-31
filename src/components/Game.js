09ecaf6210ad8b55adb4c4f0f1ecc3579b518bc8
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-31 14:52:41 +0100

    extract utility functions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,28 +1,12 @@
 import React, { useState } from 'react'
 import Row from './Row'
-import { splitEvery } from 'ramda'
 import update from '../game/update'
 import init from '../game/init'
-import { GAME_MODE, UNCLICKABLE_SQUARE, EMPTY_SQUARE } from '../constants'
+import parseMessage from '../utils/parseMessage'
+import parseRows from '../utils/parseRows'
 
-const createRows = (board, onClick) =>
-  splitEvery(3, board).map((row, index) => (
-    <Row key={index} row={index + 1} squares={row} onClick={onClick} />
-  ))
-
-const message = ({ isActive, messages }) =>
-  isActive ? messages.turn : messages.ending
-
-const parseBoard = ({ isActive, board }) =>
-  isActive
-    ? board
-    : board.map(square =>
-        square === EMPTY_SQUARE ? UNCLICKABLE_SQUARE : square
-      )
-
-function Game() {
-  const [game, updateGame] = useState(init(GAME_MODE))
-  const board = parseBoard(game)
+const Game = ({ mode = 'human' }) => {
+  const [game, updateGame] = useState(init(mode))
 
   const onClick = position => {
     updateGame(update(position, game))
@@ -30,9 +14,9 @@ function Game() {
 
   return (
     <div className="App">
-      <div className="board">{createRows(board, onClick)}</div>
+      <div className="board">{parseRows(game, Row, onClick)}</div>
       <div className="message">
-        <h2>{message(game)}</h2>
+        <h2>{parseMessage(game)}</h2>
       </div>
     </div>
   )
--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const PLAYER_NOUGHT_MARK = 'O'
 export const EMPTY_SQUARE = null
 export const UNCLICKABLE_SQUARE = ''
 export const GAME_MODE = 'ai'
+export const BOARD_SIZE = 9
