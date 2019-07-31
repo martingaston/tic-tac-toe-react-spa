8958773b3bd54d1301f6a0547ead065527324e49
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-07-31 14:55:50 +0100

    add game mode constant as app dependency injection

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './global.css'
 import Game from './components/Game'
+import { GAME_MODE } from './constants'
 
-ReactDOM.render(<Game />, document.getElementById('root'))
+ReactDOM.render(<Game mode={GAME_MODE} />, document.getElementById('root'))
