f5101a356c3a4fc6f3acc4a9f7c9343807c74deb
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-08-01 14:34:09 +0100

    add additional tests for Game.test and extract playGame logic

--- src/components/Game.test.js
+++ src/components/Game.test.js
@@ -10,16 +10,40 @@ describe('a Game component', () => {
       const crossMoves = [1, 2, 3]
       const noughtMoves = [4, 5]
 
-      combineAlternating(crossMoves, noughtMoves).forEach(move =>
-        fireEvent.click(getByLabelText(`Square ${move}`))
-      )
+      playGame(crossMoves, noughtMoves, getByLabelText)
 
       expect(getByText('Player X wins!')).toBeInTheDocument()
+    })
+
+    it('can play a complete game where O wins', () => {
+      const { getByText, getByLabelText } = render(<Game mode="human" />)
+      const crossMoves = [1, 2, 4]
+      const noughtMoves = [3, 5, 7]
+
+      playGame(crossMoves, noughtMoves, getByLabelText)
+
+      expect(getByText('Player O wins!')).toBeInTheDocument()
+    })
+
+    it('can play a complete game that ends in a draw', () => {
+      const { getByText, getByLabelText } = render(<Game mode="human" />)
+      const crossMoves = [1, 2, 7, 6, 9]
+      const noughtMoves = [5, 3, 4, 8]
+
+      playGame(crossMoves, noughtMoves, getByLabelText)
+
+      expect(getByText(/It's a draw!/)).toBeInTheDocument()
     })
   })
 })
 
 afterEach(cleanup)
+
+const playGame = (crossMoves, noughtMoves, query) => {
+  combineAlternating(crossMoves, noughtMoves).forEach(move =>
+    fireEvent.click(query(`Square ${move}`))
+  )
+}
 
 const combineAlternating = (current, alternate, combined = []) => {
   if (current.length === 0) {
